e51456389b8e43227f276f967d8e19af7bea9e83
GeeksforGeeks <raihanirvana13@gmail.com>
2023-07-05 10:53:56 +0700

    mongodb

--- src/configs/mongodb.js
+++ src/configs/mongodb.js
@@ -1,7 +1,7 @@
 const { mongoPass } = require("./environment");
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `mongodb+srv://admin-kopi:${mongoPass}@kopi-toko.vvodqmh.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://adminkopi:${mongoPass}@db-tokokopi.vtmkrgm.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
